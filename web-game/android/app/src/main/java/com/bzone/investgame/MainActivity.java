5f9118b5bfafc4faf17ac6064502813f3370cb39
Makar200410 <wwatlet13@gmail.com>
2025-11-30 07:12:46 +0300

    Remove UI and Android client code, keeping only backend server

--- web-game/android/app/src/main/java/com/bzone/investgame/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.bzone.investgame;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- web-game/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.bzone.investgame',
-  appName: 'Invest Game',
-  webDir: 'dist'
-};
-
-export default config;
